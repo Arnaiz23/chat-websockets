dac6ebc6571de9fbb3b9fa8c9fe1968b5b6f9cb6
Arnaiz23 <aaf110501@gmail.com>
2023-10-12 21:33:58 +0200

    feat ✨: Create the chat connection

---
+++ client/js/index.js
@@ -0,0 +1,21 @@
+import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js"
+
+const socket = io()
+
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const messages = document.getElementById("messages")
+
+socket.on("chat message", ({ message }) => {
+  messages.insertAdjacentHTML("beforeend", `<li>${message}</li>`)
+})
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  if (input.value) {
+    socket.emit("chat message", { message: input.value })
+    input.value = ""
+  }
+})
+
--- server/index.js
+++ server/index.js
@@ -1,18 +1,36 @@
 import express from "express"
 import dotenv from "dotenv"
+import { Server } from "socket.io"
+import { createServer } from 'node:http'
+import logger from "morgan"
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 
-app.use(express.static('client'))
+io.on("connection", async (socket) => {
+  console.log(`user connected: ${socket.id}`)
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected: ${socket.id}`)
+  })
+
+  socket.on("chat message", async ({message}) => {
+    io.emit("chat message", { message })
+  })
+})
+
+app.use(express.static("client"))
+app.use(logger('dev'))
 
 // API
 app.get("/api/", (req, res) => {
   res.send("<h2>Api</h2>")
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🚀 Server running in port: ${PORT}`)
 })
